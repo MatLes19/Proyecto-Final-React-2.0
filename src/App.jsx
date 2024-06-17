2cc320ae61c782444194daea4163dd8dcae8d1a5
Mateo Sales <mateo.99.sales@gmail.com>
2024-06-17 19:54:33 -0300

    modificacion del codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 //import imagenes from "./img/logo.png" /* "esto ingresar en el div al momento de ingresar una imagen" <img width={200} src={ imagenes } alt="" /> */
-import NavBar from "./componentes/NavBar"
-import ItemListContainer from './componentes/ItemListContainer'
+import NavBar from "./componentes/NavBar/NavBar"
+import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
   return(
     <div>
       <NavBar/>
-      <ItemListContainer />
+      <ItemListContainer saludo ="Bienvenidos a Cosas de Locas"/>
     </div>
   )
 }
---
+++ src/componentes/NavBar/CarWidget.jsx
@@ -0,0 +1,13 @@
+import { RiShoppingCart2Fill } from "react-icons/ri";
+
+
+const CarWidget = () => {
+  return (
+    <div>
+      <RiShoppingCart2Fill size={75} color="black"/>
+      <p>1</p>
+    </div>
+  )
+}
+
+export default CarWidget
